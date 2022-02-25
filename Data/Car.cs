45b1b2fdb2d2d055a2adfd58eb009fd277dd798f
Luan Macêdo <code.luan.macedo@outlook.com>
2022-02-25 14:23:21 -0300

    nova layer

---
+++ Data/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
