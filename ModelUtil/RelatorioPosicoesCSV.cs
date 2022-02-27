d0867ef58ca1d6237fb349b1854bcc4c84d15f16
Luan Macêdo <code.luan.macedo@outlook.com>
2022-02-27 17:07:29 -0300

    novos tipos do arquivo

--- ModelUtil/RelatorioPosicoesCSV.cs
+++ ModelUtil/RelatorioPosicoesCSV.cs
@@ -6,29 +6,20 @@ namespace Data
     public class RelatorioPosicoesCSV
     {
         public int Id;
-        public int Dataposicao;
-        public int Velocade;
+        public string Dataposicao;
+        public decimal Velocade;
         public int Ignicao;
-        public int Rua;
+        public string Rua;
         public int Numero;
-        public int Bairro;
-        public int Cade;
-        public int UF;
-        public int Temp1;
-        public int Temp2;
-        public int Temp3;
-        public int GPS;
+        public string Bairro;
+        public string Cidade;
+        public string UF;
         public int Latitude;
         public int Longitude;
         public int Descricao;
         public int Horimetro;
         public int Hodometro;
         public int Bateria;
-        public int Satelite;
-        public int Bloqueio;
-        public int Memoria;
-        public int CargaBateria;
-        public int Ima;
         public int DataGeracaoRelatorio;
         public int Placa;
     }
